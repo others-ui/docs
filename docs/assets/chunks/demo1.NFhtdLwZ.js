b7d09f1149646e638a0fe5e32199ba4af88717c8
yanyunwu <2480721346@qq.com>
2024-03-14 17:30:22 +0800

    docs update

---
+++ docs/assets/chunks/demo1.NFhtdLwZ.js
@@ -0,0 +1,9 @@
+const t=`<div>
+  <ot-space direction="vertical">
+    <ot-button block type="primary">默认</ot-button>
+    <ot-button block type="success">默认</ot-button>
+    <ot-button block type="warn">默认</ot-button>
+    <ot-button block type="error">默认</ot-button>
+  </ot-space>
+</div>
+`;export{t as default};
---
+++ docs/assets/chunks/demo2.FOeNDHyN.js
@@ -0,0 +1,9 @@
+const t=`<div>
+  <ot-space direction="vertical">
+    <ot-button block disabled>默认禁用</ot-button>
+    <ot-button block type="primary">默认</ot-button>
+    <ot-button block type="primary" size="small">small</ot-button>
+    <ot-button block type="primary" size="large">large</ot-button>
+  </ot-space>
+</div>
+`;export{t as default};
